a3b719753e2b5d44c786b10f64e1416ce0745f2d
takamasa moriyama <ymca0525a@gmail.com>
2019-12-03 22:39:02 +0000

    add search

--- app/models/memo.rb
+++ app/models/memo.rb
@@ -2,7 +2,7 @@ class Memo < ApplicationRecord
     belongs_to :category
     def self.search(search)
       if search
-        Memo.where(['title LIKE ? or content LIKE ?', "%#{search}%", "%#{search}%"])
+        Memo.where(['title LIKE ? or body LIKE ?', "%#{search}%", "%#{search}%"])
       else
         Memo.all
       end
